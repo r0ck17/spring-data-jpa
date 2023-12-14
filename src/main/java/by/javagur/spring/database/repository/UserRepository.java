9b4cc415dcc93d5d852a1016c3ebf1de2bb53120
Evgeniy <paut.evg@yandex.ru>
2023-12-15 00:22:52 +0300

    add more methods to user repository

--- src/main/java/by/javagur/spring/database/repository/UserRepository.java
+++ src/main/java/by/javagur/spring/database/repository/UserRepository.java
@@ -3,10 +3,8 @@ package by.javagur.spring.database.repository;
 import by.javagur.spring.database.entity.Role;
 import by.javagur.spring.database.entity.User;
 import by.javagur.spring.dto.IPersonalInfo;
-import by.javagur.spring.dto.PersonalInfo;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
@@ -15,7 +13,6 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
@@ -26,8 +23,6 @@ public interface UserRepository extends JpaRepository<User, Long> {
     List<IPersonalInfo> findAllByCompanyId(Integer companyId);
 
     Page<User> findAllBy(Pageable pageable);
-
-    List<User> findFirst3By(Sort sort);
 
     @Query("select u from User u " +
             "where u.firstname like %:firstname% and u.lastname like %:lastname%")
@@ -45,4 +40,8 @@ public interface UserRepository extends JpaRepository<User, Long> {
 
     @Query("from User u where u.role = :role and year(u.birthDate) between :yearFrom and :yearTo")
     List<User> findUsersByRoleEqualsAndAndBirthDateBetween(Role role, Integer yearFrom, Integer yearTo);
+
+    List<User> findFirst4By(Sort sort);
+
+    List<User> findAllByRole(Role role, Pageable pageable);
 }
--- src/test/java/by/javaguru/integration/repository/UserRepositoryTest.java
+++ src/test/java/by/javaguru/integration/repository/UserRepositoryTest.java
@@ -1,10 +1,8 @@
 package by.javaguru.integration.repository;
 
-import by.javagur.spring.database.entity.Company;
 import by.javagur.spring.database.entity.Role;
 import by.javagur.spring.database.entity.User;
 import by.javagur.spring.database.repository.UserRepository;
-import by.javagur.spring.dto.PersonalInfo;
 import by.javaguru.annotation.IT;
 import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Test;
@@ -12,6 +10,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
+import java.awt.*;
 import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -41,16 +40,6 @@ public class UserRepositoryTest {
 
     }
 
-
-    @Test
-    void findFirst3By() {
-        var users = userRepository.findFirst3By(Sort.by("firstname")
-                .and(Sort.by("firstname")).descending());
-        assertTrue(!users.isEmpty());
-        assertThat(users).hasSize(3);
-
-    }
-
     @Test
     void updateRoleTest() {
         var entity1 = userRepository.findById(1L);
@@ -66,5 +55,74 @@ public class UserRepositoryTest {
         var user = userRepository.findAllByFirstnameContainingAndLastnameContaining("a", "a");
         assertFalse(user.isEmpty());
         assertThat(user).hasSize(3);
+    }
+
+    @Test
+    void findFirst4SortedByBirthdate() {
+        List<User> firstUsers = userRepository.findFirst4By(Sort.by("birthDate"));
+
+        assertThat(firstUsers).hasSize(4);
+
+        List<String> expectedNames = List.of("Vlad", "Kate", "Ivan", "Petr");
+
+        List<String> actualNames = firstUsers.stream()
+                .map(User::getFirstname)
+                .toList();
+
+        assertThat(actualNames).isEqualTo(expectedNames);
+    }
+
+    @Test
+    void findFirst4SortedByBirthdateAndFio() {
+        List<User> firstUsers = userRepository.findFirst4By(Sort.by("birthDate", "firstname", "lastname"));
+
+        assertThat(firstUsers).hasSize(4);
+
+        List<String> expectedNames = List.of("Kate", "Vlad", "Ivan", "Petr");
+
+        List<String> actualNames = firstUsers.stream()
+                .map(User::getFirstname)
+                .toList();
+
+        assertThat(actualNames).isEqualTo(expectedNames);
+    }
+
+    @Test
+    void findFirst4AdminById() {
+        PageRequest pageRequest = PageRequest.of(0, 4, Sort.by("id"));
+        List<User> users = userRepository.findAllByRole(Role.ADMIN, pageRequest);
+
+        List<String> actualNames = users.stream()
+                .map(User::getFirstname)
+                .toList();
+        List<String> expectedNames = List.of("Ivan", "Kate");
+
+        assertEquals(expectedNames, actualNames);
+    }
+
+    @Test
+    void findFirst2UserByFirstName() {
+        PageRequest pageRequest = PageRequest.of(0, 2, Sort.by("firstname"));
+        List<User> users = userRepository.findAllByRole(Role.USER, pageRequest);
+
+        List<String> actualNames = users.stream()
+                .map(User::getFirstname)
+                .toList();
+        List<String> expectedNames = List.of("Petr", "Sveta");
+
+        assertEquals(expectedNames, actualNames);
+    }
+
+    @Test
+    void findSecond2UserByLastName() {
+        PageRequest pageRequest = PageRequest.of(1, 2, Sort.by("lastname"));
+        List<User> users = userRepository.findAllByRole(Role.USER, pageRequest);
+
+        List<String> actualNames = users.stream()
+                .map(User::getFirstname)
+                .toList();
+        List<String> expectedNames = List.of("Vlad");
+
+        assertEquals(expectedNames, actualNames);
     }
 }
